0165df6b7516a11ae6c3340eab2cbca172ea6365
Filippo Vicari <fil.vicari@gmail.com>
2019-08-27 10:30:04 +0200

    Iniziamo!

---
+++ statistica.php
@@ -0,0 +1,18 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <p>
+    When $a \ne 0$, there are two solutions to \(ax^2 + bx + c = 0\) and they are
+
+<span style="font-size:5rem"> $$x = {-b \pm \sqrt{b^2-4ac} \over 2a}$$ </span>
+    </p>
+</body>
+<script type="text/javascript" async
+  src="https://cdnjs.cloudflare.com/ajax/libs/mathjax/2.7.5/MathJax.js?config=TeX-MML-AM_CHTML">
+</script>
+</html>
