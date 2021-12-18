d1f610c6f253382cbba9bede5a0e3d7e8cedafef
filvi <fil.vicari@gmail.com>
2021-12-18 01:24:24 +0100

    working on env file to connect to database

--- controller/Database.php
+++ controller/Database.php
@@ -1,6 +1,15 @@
 <?php
+include_once(__DIR__ . "/dontenvreader.php");
 
+use DevCoder\DotEnv;
 
+(new DotEnv(__DIR__ . '/.env'))->load();
+
+echo getenv('APP_ENV');
+echo "<br>";
+// dev
+echo getenv('DATABASE_DNS')
+// mysql:host=localhost;dbname=test;
 
 
 ?>
